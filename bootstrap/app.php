8c74cc44ee37771df1534c7eedd778b7dd91136c
Davina Leong <leong.shi.yun@gmail.com>
2026-01-23 15:43:41 +0800

    Enforce HTTPS in production with trusted proxies for Heroku

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all proxies for Heroku HTTPS detection
+        $middleware->trustProxies(at: '*');
+        
         $middleware->web(append: [
             \App\Http\Middleware\ShareActiveTheme::class,
         ]);
