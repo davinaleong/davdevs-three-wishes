7ccc49d338868b875289686636a97b7d622698ad
Davina Leong <leong.shi.yun@gmail.com>
2026-01-31 22:07:20 +0800

    Fix Heroku deployment: remove lucide dependency, add Node.js version specification, and use inline SVG icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
-import { icons } from 'lucide';
 
 window.Alpine = Alpine;
 
@@ -28,16 +27,17 @@ Alpine.data('passwordToggle', () => ({
             const button = this.$el.querySelector('.password-toggle-btn');
             if (button) {
                 const iconContainer = button.querySelector('.lucide-icon');
-                if (iconContainer && icons[this.iconName]) {
-                    iconContainer.innerHTML = icons[this.iconName].toSvg({ 
-                        size: 20, 
-                        color: 'black',
-                        class: 'transition-colors duration-200'
-                    });
+                if (iconContainer) {
+                    // Simple inline SVG icons instead of lucide dependency
+                    const eyeIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="transition-colors duration-200"><path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"/><circle cx="12" cy="12" r="3"/></svg>';
+                    const eyeOffIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="transition-colors duration-200"><path d="m9.88 9.88 4.24 4.24"/><path d="m10.73 5.08-1.24 1.24A11.46 11.46 0 0 1 12 6c7 0 11 6 11 6a11.77 11.77 0 0 1-1.17 1.42l.95.95a1 1 0 1 1-1.41 1.41L3.71 2.22a1 1 0 0 1 1.41-1.41l5.66 5.66Z"/></svg>';
+                    iconContainer.innerHTML = this.show ? eyeOffIcon : eyeIcon;
                 }
             }
         });
     }
 }));
+
+Alpine.start();
 
 Alpine.start();
